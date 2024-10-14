d6b2c4089b6bb5fc078254780c9e61c7ad13d953
LeeYooBin <santosroberdan25@gmail.com>
2024-10-14 12:12:39 -0300

    feat: adding scheduler to consume command queue every 10 seconds

---
+++ src/main/java/com/example/websocket_service/service/ScheduledSqsConsumer.java
@@ -0,0 +1,21 @@
+package com.example.websocket_service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduledSqsConsumer {
+  @Autowired
+  private SqsService sqsService;
+
+  @Scheduled(fixedRate = 10000)
+  public void run() {
+    try {
+      sqsService.consumeCommandQueue();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
